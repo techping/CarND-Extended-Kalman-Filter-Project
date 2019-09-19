dfceb75b20077641b22138494a84367b3e3e0e02
Ziping Chen <techping.chan@gmail.com>
2019-09-19 01:06:37 -0700

    [ekf] commit my implementation of kf and ekf
    Signed-off-by: Ziping Chen <techping.chan@gmail.com>

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -26,16 +26,53 @@ void KalmanFilter::Predict() {
   /**
    * TODO: predict the state
    */
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
   /**
    * TODO: update the state by using Kalman Filter equations
    */
+  VectorXd y = z - H_ * x_;
+  MatrixXd S = H_ * P_ * H_.transpose() + R_;
+  MatrixXd K = P_ * H_.transpose() * S.inverse();
+
+  x_ = x_ + K * y;
+  MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+  P_ = (I - K * H_) * P_;
 }
+
+#include "tools.h"
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
    * TODO: update the state by using Extended Kalman Filter equations
    */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+  if (px < 1e-8 && py < 1e-8)
+    return;
+  MatrixXd Hj = Tools().CalculateJacobian(x_);
+  VectorXd hofx(3);
+  float rho = sqrt(px * px + py * py);
+  hofx << rho, atan2(py, px), (px * vx + py * vy) / rho;
+  
+  VectorXd y = z - hofx;
+  // normalize the angle between -pi to pi
+  while (y(1) > M_PI) {
+     y(1) -= 2 * M_PI;
+  }
+  while (y(1) < -M_PI) {
+     y(1) += 2 * M_PI;
+  }
+
+  MatrixXd S = Hj * P_ * Hj.transpose() + R_;
+  MatrixXd K = P_ * Hj.transpose() * S.inverse();
+
+  x_ = x_ + K * y;
+  MatrixXd I = MatrixXd::Identity(x_.size(), x_.size());
+  P_ = (I - K * Hj) * P_;
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -14,6 +14,14 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   /**
    * TODO: Calculate the RMSE here.
    */
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+  for (int i = 0; i < estimations.size(); ++i) {
+    rmse += VectorXd((estimations[i] - ground_truth[i]).array() * (estimations[i] - ground_truth[i]).array());
+  }
+  rmse /= estimations.size();
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -21,4 +29,17 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
    * TODO:
    * Calculate a Jacobian here.
    */
+  MatrixXd Hj(3, 4);
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+  if (px < 1e-8 && py < 1e-8) {
+    std::cout << "[error] px and py are 0" << std::endl;
+    return Hj;
+  }
+  Hj << px / sqrt(px * px + py * py), py / sqrt(px * px + py * py), 0, 0,
+     -py / (px * px + py * py), px / (px * px + py * py), 0, 0,
+     py * (vx * py - vy * px) / pow(px * px + py * py, 1.5), px * (vy * px - vx * py) / pow(px * px + py * py, 1.5), px / sqrt(px * px + py * py), py / sqrt(px * px + py * py);
+  return Hj;
 }
